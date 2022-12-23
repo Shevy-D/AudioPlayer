6ec4fd14bf368763ffac6c11b1f4a99143ac0298
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-12-23 14:49:43 +0500

    Added splash screen

--- app/src/main/java/com/shevy/audioplayer/MainActivity.kt
+++ app/src/main/java/com/shevy/audioplayer/MainActivity.kt
@@ -14,6 +14,8 @@ class MainActivity : AppCompatActivity() {
     private var handler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.Theme_AudioPlayer)
+
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
