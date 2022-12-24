5614fe711489585b87e8379e7bae1495d7729624
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-12-24 18:11:21 +0500

    Created PlaylistActivity & FavoriteActivity

--- app/src/main/java/com/shevy/audioplayer/MainActivity.kt
+++ app/src/main/java/com/shevy/audioplayer/MainActivity.kt
@@ -1,10 +1,9 @@
 package com.shevy.audioplayer
 
-import android.media.MediaPlayer
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.widget.SeekBar
+import androidx.appcompat.app.AppCompatActivity
 import com.shevy.audioplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,45 +19,57 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.music)
-
-        val seekBar = binding.seekBar
-        seekBar.progress = 0
-        seekBar.max = mediaPlayer.duration
-
-        binding.playBtn.setOnClickListener {
-            if (!mediaPlayer.isPlaying) {
-                mediaPlayer.start()
-                binding.playBtn.setImageResource(R.drawable.ic_pause)
-            } else {
-                mediaPlayer.pause()
-                binding.playBtn.setImageResource(R.drawable.ic_play)
-            }
+        binding.shuffleBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, PlayerActivity::class.java))
         }
 
-        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
-            override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
-                if (changed) {
-                    mediaPlayer.seekTo(pos)
-                }
-            }
-
-            override fun onStartTrackingTouch(p0: SeekBar?) {
-            }
-
-            override fun onStopTrackingTouch(p0: SeekBar?) {
-            }
-        })
-
-        runnable = Runnable {
-            seekBar.progress = mediaPlayer.currentPosition
-            handler.postDelayed(runnable, 1000)
+        binding.favouriteBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, FavoriteActivity::class.java))
         }
-        handler.postDelayed(runnable, 1000)
 
-        mediaPlayer.setOnCompletionListener {
-            binding.playBtn.setImageResource(R.drawable.ic_play)
-            seekBar.progress = 0
+        binding.playlistBtn.setOnClickListener {
+            startActivity(Intent(this@MainActivity, PlaylistActivity::class.java))
         }
+
+        /*       val mediaPlayer: MediaPlayer = MediaPlayer.create(this, R.raw.music)
+
+               val seekBar = binding.seekBar
+               seekBar.progress = 0
+               seekBar.max = mediaPlayer.duration
+
+               binding.playBtn.setOnClickListener {
+                   if (!mediaPlayer.isPlaying) {
+                       mediaPlayer.start()
+                       binding.playBtn.setImageResource(R.drawable.ic_pause)
+                   } else {
+                       mediaPlayer.pause()
+                       binding.playBtn.setImageResource(R.drawable.ic_play)
+                   }
+               }
+
+               seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+                   override fun onProgressChanged(p0: SeekBar?, pos: Int, changed: Boolean) {
+                       if (changed) {
+                           mediaPlayer.seekTo(pos)
+                       }
+                   }
+
+                   override fun onStartTrackingTouch(p0: SeekBar?) {
+                   }
+
+                   override fun onStopTrackingTouch(p0: SeekBar?) {
+                   }
+               })
+
+               runnable = Runnable {
+                   seekBar.progress = mediaPlayer.currentPosition
+                   handler.postDelayed(runnable, 1000)
+               }
+               handler.postDelayed(runnable, 1000)
+
+               mediaPlayer.setOnCompletionListener {
+                   binding.playBtn.setImageResource(R.drawable.ic_play)
+                   seekBar.progress = 0
+               }*/
     }
 }
