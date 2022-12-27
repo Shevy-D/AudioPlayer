468a006d3b8130dc72b67e67753b227a715f5449
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-12-27 20:08:55 +0500

    Added BroadcastReceiver

--- app/src/main/java/com/shevy/audioplayer/MusicService.kt
+++ app/src/main/java/com/shevy/audioplayer/MusicService.kt
@@ -1,5 +1,7 @@
 package com.shevy.audioplayer
 
+import android.annotation.SuppressLint
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.graphics.BitmapFactory
@@ -27,7 +29,21 @@ class MusicService : Service() {
         }
     }
 
+    @SuppressLint("UnspecifiedImmutableFlag")
     fun showNotification(){
+
+        val prevIntent = Intent(baseContext, NotificationReceiver::class.java).setAction(ApplicationClass.PREVIOUS)
+        val prevPendingIntent = PendingIntent.getBroadcast(baseContext, 0, prevIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val playIntent = Intent(baseContext, NotificationReceiver::class.java).setAction(ApplicationClass.PLAY)
+        val playPendingIntent = PendingIntent.getBroadcast(baseContext, 0, playIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val nextIntent = Intent(baseContext, NotificationReceiver::class.java).setAction(ApplicationClass.NEXT)
+        val nextPendingIntent = PendingIntent.getBroadcast(baseContext, 0, nextIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val exitIntent = Intent(baseContext, NotificationReceiver::class.java).setAction(ApplicationClass.EXIT)
+        val exitPendingIntent = PendingIntent.getBroadcast(baseContext, 0, exitIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
         val notification = NotificationCompat.Builder(baseContext, ApplicationClass.CHANNEL_ID)
             .setContentTitle(PlayerActivity.musicListPA[PlayerActivity.songPosition].title)
             .setContentText(PlayerActivity.musicListPA[PlayerActivity.songPosition].artist)
@@ -37,10 +53,10 @@ class MusicService : Service() {
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
             .setOnlyAlertOnce(true)
-            .addAction(R.drawable.ic_previous, "Previous", null)
-            .addAction(R.drawable.ic_play, "Play", null)
-            .addAction(R.drawable.ic_next, "Next", null)
-            .addAction(R.drawable.ic_exit, "Exit", null)
+            .addAction(R.drawable.ic_previous, "Previous", prevPendingIntent)
+            .addAction(R.drawable.ic_play, "Play", playPendingIntent)
+            .addAction(R.drawable.ic_next, "Next", nextPendingIntent)
+            .addAction(R.drawable.ic_exit, "Exit", exitPendingIntent)
             .build()
 
         startForeground(13, notification)
