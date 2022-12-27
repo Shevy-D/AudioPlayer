ae97fc82c74f653014c42da4cc2d290b5ec50a8d
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-12-27 19:28:13 +0500

    Created Notification

--- app/src/main/java/com/shevy/audioplayer/MusicService.kt
+++ app/src/main/java/com/shevy/audioplayer/MusicService.kt
@@ -2,16 +2,22 @@ package com.shevy.audioplayer
 
 import android.app.Service
 import android.content.Intent
+import android.graphics.BitmapFactory
 import android.media.MediaPlayer
 import android.os.Binder
 import android.os.IBinder
+import android.support.v4.media.session.MediaSessionCompat
+import androidx.core.app.NotificationCompat
+import com.shevy.audioplayer.presentation.PlayerActivity
 
 class MusicService : Service() {
 
     private var myBinder = MyBinder()
     var mediaPlayer: MediaPlayer? = null
+    private lateinit var mediaSession : MediaSessionCompat
 
-    override fun onBind(p0: Intent?): IBinder? {
+    override fun onBind(p0: Intent?): IBinder {
+        mediaSession = MediaSessionCompat(baseContext, "My Music")
         return myBinder
     }
 
@@ -20,4 +26,24 @@ class MusicService : Service() {
             return this@MusicService
         }
     }
+
+    fun showNotification(){
+        val notification = NotificationCompat.Builder(baseContext, ApplicationClass.CHANNEL_ID)
+            .setContentTitle(PlayerActivity.musicListPA[PlayerActivity.songPosition].title)
+            .setContentText(PlayerActivity.musicListPA[PlayerActivity.songPosition].artist)
+            .setSmallIcon(R.drawable.ic_music_icon)
+            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.app_icon))
+            .setStyle(androidx.media.app.NotificationCompat.MediaStyle().setMediaSession(mediaSession.sessionToken))
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setOnlyAlertOnce(true)
+            .addAction(R.drawable.ic_previous, "Previous", null)
+            .addAction(R.drawable.ic_play, "Play", null)
+            .addAction(R.drawable.ic_next, "Next", null)
+            .addAction(R.drawable.ic_exit, "Exit", null)
+            .build()
+
+        startForeground(13, notification)
+    }
+
 }
