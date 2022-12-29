8c49e5786c5939aaf9bf879045a20000ceaf54b5
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2022-12-29 18:17:37 +0500

    Added AlertDialog

--- app/src/main/java/com/shevy/audioplayer/presentation/MainActivity.kt
+++ app/src/main/java/com/shevy/audioplayer/presentation/MainActivity.kt
@@ -3,15 +3,18 @@ package com.shevy.audioplayer.presentation
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.shevy.audioplayer.MusicAdapter
 import com.shevy.audioplayer.R
 import com.shevy.audioplayer.databinding.ActivityMainBinding
@@ -65,7 +68,26 @@ class MainActivity : AppCompatActivity() {
                 R.id.navSettings -> Toast.makeText(baseContext, "Setting", Toast.LENGTH_SHORT)
                     .show()
                 R.id.navAbout -> Toast.makeText(baseContext, "About", Toast.LENGTH_SHORT).show()
-                R.id.navExit -> exitProcess(1)
+                R.id.navExit -> {
+                    val builder = MaterialAlertDialogBuilder(this)
+                    builder.setTitle("Exit")
+                        .setMessage("Do you want to close app?")
+                        .setPositiveButton("Yes") { _, _ ->
+                            if (PlayerActivity.musicService != null) {
+                                PlayerActivity.musicService!!.stopForeground(true)
+                                PlayerActivity.musicService!!.mediaPlayer!!.release()
+                                PlayerActivity.musicService = null
+                            }
+                            exitProcess(1)
+                        }
+                        .setNegativeButton("No") { dialog, _ ->
+                            dialog.dismiss()
+                        }
+                    val customDialog = builder.create()
+                    customDialog.show()
+                    customDialog.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED)
+                    customDialog.getButton(AlertDialog.BUTTON_NEGATIVE).setTextColor(Color.RED)
+                }
             }
             true
         }
@@ -183,7 +205,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        if(!PlayerActivity.isPlaying && PlayerActivity.musicService != null){
+        if (!PlayerActivity.isPlaying && PlayerActivity.musicService != null) {
             PlayerActivity.musicService!!.stopForeground(true)
             PlayerActivity.musicService!!.mediaPlayer!!.release()
             PlayerActivity.musicService = null
