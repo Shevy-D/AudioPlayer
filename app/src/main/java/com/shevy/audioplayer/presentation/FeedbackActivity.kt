fba8a3d49298155d648720504ac259bfaacff18f
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-02-08 23:15:59 +0500

    Created Feedback feature

--- app/src/main/java/com/shevy/audioplayer/presentation/FeedbackActivity.kt
+++ app/src/main/java/com/shevy/audioplayer/presentation/FeedbackActivity.kt
@@ -1,10 +1,15 @@
 package com.shevy.audioplayer.presentation
 
-
+import android.content.Context
+import android.net.ConnectivityManager
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.shevy.audioplayer.databinding.ActivityFeedbackBinding
+import java.util.*
+import javax.mail.*
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
 
 class FeedbackActivity : AppCompatActivity() {
 
@@ -16,12 +21,38 @@ class FeedbackActivity : AppCompatActivity() {
         setContentView(binding.root)
         supportActionBar?.title = "Feedback"
 
-        val feedbackMsg =
-            binding.feedbackMsgFA.text.toString() + "\n" + binding.emailFA.text.toString()
-        val subject = binding.topicFA.text.toString()
-
         binding.sendFA.setOnClickListener {
-            Toast.makeText(this, feedbackMsg, Toast.LENGTH_SHORT).show()
+            val feedbackMsg =
+                binding.feedbackMsgFA.text.toString() + "\n" + binding.emailFA.text.toString()
+            val subject = binding.topicFA.text.toString()
+            val userName = "ermolay007@gmail.com"
+            val pass = "ermolay007"
+            val cm = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+            if(feedbackMsg.isNotEmpty() && subject.isNotEmpty() && (cm.activeNetworkInfo?.isConnectedOrConnecting == true)){
+                Thread{
+                    try {
+                        val properties = Properties()
+                        properties["mail.smtp.auth"] = "true"
+                        properties["mail.smtp.starttls.enable"] = "true"
+                        properties["mail.smtp.host"] = "smtp.gmail.com"
+                        properties["mail.smtp.port"] = "587"
+                        val session = Session.getInstance(properties, object : Authenticator(){
+                            override fun getPasswordAuthentication(): PasswordAuthentication {
+                                return PasswordAuthentication(userName, pass)
+                            }
+                        })
+                        val mail = MimeMessage(session)
+                        mail.subject = subject
+                        mail.setText(feedbackMsg)
+                        mail.setFrom(InternetAddress(userName))
+                        mail.setRecipients(Message.RecipientType.TO, InternetAddress.parse(userName))
+                        Transport.send(mail)
+                    }catch (e: Exception){Toast.makeText(this, e.toString(), Toast.LENGTH_SHORT).show()}
+                }.start()
+                Toast.makeText(this, "Thanks For Feedback!!", Toast.LENGTH_SHORT).show()
+                finish()
+            }
+            else Toast.makeText(this, "Went Something Wrong!!", Toast.LENGTH_SHORT).show()
         }
     }
 }
