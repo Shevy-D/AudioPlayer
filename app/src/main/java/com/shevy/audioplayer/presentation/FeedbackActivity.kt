2158f052ac14526a4e47f170e14906e391ec8528
Dmitriy Shevyakov <dmitriy_shevyakov@mail.ru>
2023-02-08 22:50:23 +0500

    Created Feedback feature

--- app/src/main/java/com/shevy/audioplayer/presentation/FeedbackActivity.kt
+++ app/src/main/java/com/shevy/audioplayer/presentation/FeedbackActivity.kt
@@ -1,6 +1,8 @@
 package com.shevy.audioplayer.presentation
 
+
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.shevy.audioplayer.databinding.ActivityFeedbackBinding
 
@@ -13,5 +15,13 @@ class FeedbackActivity : AppCompatActivity() {
         binding = ActivityFeedbackBinding.inflate(layoutInflater)
         setContentView(binding.root)
         supportActionBar?.title = "Feedback"
+
+        val feedbackMsg =
+            binding.feedbackMsgFA.text.toString() + "\n" + binding.emailFA.text.toString()
+        val subject = binding.topicFA.text.toString()
+
+        binding.sendFA.setOnClickListener {
+            Toast.makeText(this, feedbackMsg, Toast.LENGTH_SHORT).show()
+        }
     }
 }
